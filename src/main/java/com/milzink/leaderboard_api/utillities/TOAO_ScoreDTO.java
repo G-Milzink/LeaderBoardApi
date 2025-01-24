1fc6921ec509219f38839af1d49ecf668261d39c
Gustaaf Milzink <G.Milzink@gmail.com>
2025-01-24 16:21:52 +0100

    add timestamp field to constructor(whoops)

--- src/main/java/com/milzink/leaderboard_api/utillities/TOAO_ScoreDTO.java
+++ src/main/java/com/milzink/leaderboard_api/utillities/TOAO_ScoreDTO.java
@@ -1,16 +1,28 @@
 package com.milzink.leaderboard_api.utillities;
 
+import java.time.LocalDateTime;
+
 public class TOAO_ScoreDTO {
 
     private String playerId;
     private Integer score;
+    private LocalDateTime timestamp;
 
     public TOAO_ScoreDTO() {
     }
 
-    public TOAO_ScoreDTO(String playerId, int score) {
+    public TOAO_ScoreDTO(String playerId, int score, LocalDateTime timestamp) {
         this.playerId = playerId;
         this.score = score;
+        this.timestamp = timestamp;
+    }
+
+    public void setPlayerId(String playerId) {
+        this.playerId = playerId;
+    }
+
+    public String getPlayerId() {
+        return playerId;
     }
 
     public void setScore(int score) {
@@ -21,12 +33,12 @@ public class TOAO_ScoreDTO {
         return score;
     }
 
-    public void setPlayerId(String playerId) {
-        this.playerId = playerId;
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
     }
 
-    public String getPlayerId() {
-        return playerId;
+    public LocalDateTime getTimestamp() {
+        return timestamp;
     }
 
 }
